f045b52759bd56c622e8f31a1609fd1283270b0e
Longsight Support Account <sakaiadmin@sakaix.seminolestate.edu>
2012-09-11 09:54:51 -0400

    atd for seminole commit one

--- custom/seminole/editor/ckeditor.launch.js
+++ custom/seminole/editor/ckeditor.launch.js
@@ -31,11 +31,11 @@ sakai.editor.editors.ckeditor.launch = function(targetId, config) {
     }
     CKEDITOR.replace(targetId, {
         skin: 'v2',
-        height: 320,
+        height: 310,
         filebrowserBrowseUrl :'/library/editor/FCKeditor/editor/filemanager/browser/default/browser.html?Connector=/sakai-fck-connector/web/editor/filemanager/browser/default/connectors/jsp/connector' + folder,
         filebrowserImageBrowseUrl : '/library/editor/FCKeditor/editor/filemanager/browser/default/browser.html?Type=Image&Connector=/sakai-fck-connector/web/editor/filemanager/browser/default/connectors/jsp/connector' + folder,
         filebrowserFlashBrowseUrl :'/library/editor/FCKeditor/editor/filemanager/browser/default/browser.html?Type=Flash&Connector=/sakai-fck-connector/web/editor/filemanager/browser/default/connectors/jsp/connector' + folder,
-        extraPlugins: (sakai.editor.enableResourceSearch ? 'resourcesearch,' : '') + 'ckeditor_wiris,wordcount',
+        extraPlugins: (sakai.editor.enableResourceSearch ? 'resourcesearch,' : '') + 'ckeditor_wiris,wordcount,atd-ckeditor',
 
         // These two settings enable the browser's native spell checking and context menus.
         // Control-Right-Click (Windows/Linux) or Command-Right-Click (Mac) on highlighted words
@@ -48,12 +48,13 @@ sakai.editor.editors.ckeditor.launch = function(targetId, config) {
         toolbar_Full:
         [
             ['Source','-','Templates'],
-            ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker'],
+            ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print' ],
             ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
             ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
             '/',
             ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
             ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+	    ['atd-ckeditor'],
             ['BidiLtr', 'BidiRtl' ],
             ['Link','Unlink','Anchor'],
             (sakai.editor.enableResourceSearch
